b52192aaf231a3ecd8d0d5da7dc1e37df6328083
thanigavelan <thanigavelan1305@gmail.com>
2024-08-17 19:02:18 +0530

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,12 +4,65 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Mail\Message;
 
+use App\Models\Product;
+use App\Models\Category;
+
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class HomeController extends Controller
 {
     public function index(Request $request)
     {
-        return view('frontend/home');
+
+        $categories= Category::all();
+
+        $query = Product::query();
+
+        // Apply filters based on the request
+        if ($request->has('category') && $request->category != 'All') {
+            $query->where('category_id', $request->category);
+        }
+
+        if ($request->has('search')) {
+            $searchTerm = $request->input('search');
+            $query->where('name', 'like', "%{$searchTerm}%");
+        }
+
+        $data = [
+            'product' => $query->get(),
+            'categories' => $categories
+        ];
+
+        return view('frontend/home', $data);
+    }
+
+    public function show($id)
+    {
+        try {
+            $product = Product::findOrFail($id);
+            return view('frontend.show', compact('product'));
+        } catch (ModelNotFoundException $e) {
+            // Handle the case where the product is not found
+            return redirect()->route('products.index')->with('error', 'Product not found.');
+        }
+    }
+        
+
+    public function sendEmailManually()
+    {
+        $recipient = 'vprabhu1771@gmail.com'; // Replace with actual recipient's email
+        $subject = 'Custom Subject';
+        $body = 'This is the body of the email. You can include HTML here if needed.';
+
+        Mail::raw($body, function(Message $message) use ($recipient, $subject) {
+            $message->to($recipient);
+            $message->subject($subject);
+            // You can add attachments or other options here if needed
+        });
+
+        return "Email sent successfully!";
     }
 }
