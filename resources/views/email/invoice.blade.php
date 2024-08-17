5c17a83403e0176a6046ee6b91883c347ecd9576
thanigavelan <thanigavelan1305@gmail.com>
2024-08-17 18:29:18 +0530

    Update invoice.blade.php

---
+++ resources/views/email/invoice.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Invoice Email</title>
+</head>
+<body>
+    
+    <p>Your invoice is attached to this email.</p>
+    <!-- Add more email content here -->
+</body>
+</html>
